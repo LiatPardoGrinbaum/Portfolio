b92ff1da5bbd44e2a6b5ac4c868e0223964609e4
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-08-06 23:41:41 +0300

    added style to navbar links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header-container">
+      {/* //!add smooth scrool when click links on the same page */}
       <div className="header-inner">
         <div className="logo">
           <h2>Liat Pardo Grinbaum</h2>
