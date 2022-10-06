7d61d5bdf9a04350741640103b34eb28af4b17da
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2022-10-06 14:32:46 +0300

    css changes . about and skills section edit

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Skill from "./Skill";
+import reactIcon from "../assets/skills-icons/react-icon.png";
+import nodeIcon from "../assets/skills-icons/node-icon.png";
+import sassIcon from "../assets/skills-icons/sass-icon.png";
+import gitIcon from "../assets/skills-icons/git-icon.png";
 
 const About = () => {
   return (
@@ -19,13 +24,23 @@ const About = () => {
         </div>
         <div className="skills">
           <div className="title">
-            <h1>Skills</h1>
+            <h1>My Skills</h1>
           </div>
-          Skills icons by
+          <div className="skills-container">
+            <Skill skillImg={reactIcon} skillTitle="React" />
+            <Skill skillImg={sassIcon} skillTitle="Sass" />
+            <Skill skillImg={nodeIcon} skillTitle="Node.js" />
+            <Skill skillImg={gitIcon} skillTitle="Git" />
+            <Skill skillImg={reactIcon} skillTitle="React" />
+            <Skill skillImg={sassIcon} skillTitle="Sass" />
+            <Skill skillImg={nodeIcon} skillTitle="Node.js" />
+            <Skill skillImg={gitIcon} skillTitle="Git" />
+          </div>
+          {/* Skills icons by
           <a target="blank" href="https://icons8.com">
             Icons8
           </a>
-          <p>links</p>
+          <p>links</p>*/}
         </div>
       </div>
     </section>
---
+++ src/components/Skill.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Skill = ({ skillImg, skillTitle }) => {
+  return (
+    <div className="skill-box tooltip">
+      <img src={skillImg} alt={skillTitle} />
+
+      <div class="bottom">
+        <h3>{skillTitle}</h3>
+        <i></i>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
