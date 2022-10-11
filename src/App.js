275a2152c1667fcac6b2b076d74c88e35064aa80
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2022-10-11 12:09:56 +0300

    added portfolio page and projects component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./scss/App.css";
 import Home from "./components/Home";
 import About from "./components/About/About";
-import Portfolio from "./components/Portfolio";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
   return (
