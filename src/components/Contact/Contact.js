341fdfdad8f08123f0b270b600e8e0259aa215a7
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2022-11-02 16:08:16 +0200

    contact updated, added alert for form submission (first try)

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "@mui/material";
 
 const Contact = () => {
+  //!remove alert states and components to form component later
+  const [successAlert, setSuccessAlert] = useState(true);
+  const [failureAlert, setFailureAlert] = useState(true);
   return (
     <section id="contact">
       <div className="contact-container">
         <div className="contact-details"></div>
+        {successAlert && (
+          <Alert
+            onClose={() => {
+              setSuccessAlert(false);
+            }}>
+            This is a success alert — check it out!
+          </Alert>
+        )}
+        {failureAlert && (
+          <Alert
+            severity="error"
+            onClose={() => {
+              setFailureAlert(false);
+            }}>
+            This is a success alert — check it out!
+          </Alert>
+        )}
         <div className="contact-form"></div>
       </div>
     </section>
