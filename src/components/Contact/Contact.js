9d5ad599dcb5462d7a1a87c8860a2319e356863c
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2022-11-05 22:42:49 +0200

    added left section in contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Alert } from "@mui/material";
 import ContactUs from "./ContactUs";
+import contactme from "../../assets/contactme.jpg";
 
 const Contact = () => {
   //!remove alert states and components to form component later
@@ -18,15 +19,18 @@ const Contact = () => {
           <div className="contact-details">
             <h1>Let's get in touch!</h1>
             <div className="contact-way">
-              <i class="fa-solid fa-phone"></i> <a href="tel:+972524555527"> Call Me</a>
-            </div>
-            <div className="contact-way">
-              <i class="fa-solid fa-envelope"></i> <a href="mail:+972524555527"> Email</a>
-            </div>
-            <div className="contact-way">
+              <a href="tel:+972524555527">
+                {" "}
+                <i class="fa-solid fa-phone"></i>
+              </a>
+              <a href="mailto:+972524555527">
+                {" "}
+                <i class="fa-solid fa-envelope"></i>
+              </a>
               <i class="fa-brands fa-linkedin"></i>
               <i className="fa-brands fa-github"></i>
             </div>
+            <img src={contactme} alt="phone" width="400" style={{ opacity: "0.5" }} />
           </div>
           {/*  {successAlert && (
           <Alert
